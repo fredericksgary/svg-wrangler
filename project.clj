b683cdd92bb5a0a2b4fcfacf42e7f32e2ca855ba
Gary Fredericks <fredericksgary@gmail.com>
2013-04-30 08:32:39 -0500

    Initial

---
+++ project.clj
@@ -0,0 +1,6 @@
+(defproject com.gfredericks/svg-wrangler "0.1.0"
+  :description "Helpers for doing svg-hiccup."
+  :url "http://example.com/FIXME"
+  :license {:name "Eclipse Public License"
+            :url "http://www.eclipse.org/legal/epl-v10.html"}
+  :dependencies [[org.clojure/clojure "1.5.1"]])
---
+++ src/com/gfredericks/svg_wrangler.clj
@@ -0,0 +1,14 @@
+(ns com.gfredericks.svg-wrangler
+  (:require [clojure.string :as s]))
+
+(defn css
+  [m]
+  (s/join (for [[k v] m]
+            (format "%s:%s;" (name k) (str v)))))
+
+(defn svg*
+  [[minx miny user-width user-height :as dims] width height contents]
+  [:svg {:xmlns "http://www.w3.org/2000/svg" :version "1.1"
+         :viewBox (apply format "%f %f %f %f" (map double dims))
+         :style (format "width:%dpx;height:%dpx;" width height)}
+   contents])
