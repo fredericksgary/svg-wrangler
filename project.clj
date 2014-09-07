48e70e95dcfabf35cc27420c63bfd19b3b802767
Gary Fredericks <fredericksgary@gmail.com>
2014-09-07 16:44:48 -0500

    Develop project.clj a bit

--- project.clj
+++ project.clj
@@ -1,6 +1,6 @@
 (defproject com.gfredericks/svg-wrangler "0.1.6-SNAPSHOT"
   :description "Helpers for doing svg-hiccup."
-  :url "http://example.com/FIXME"
+  :url "https://github.com/gfredericks/svg-wrangler"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/clojure "1.5.1"]])
+  :dependencies [[org.clojure/clojure "1.6.0"]])
